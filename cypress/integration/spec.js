31d731e1c3950ddce15c9237dedc9914327f191f
Tymoteusz Czech <mail@tymek.cz>
2020-05-05 00:12:22 +0200

    build update

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,16 +4,16 @@ describe('Sapper template app', () => {
 	})
 
 	it('has the correct <h1>', () => {
-		cy.contains('h1', 'Great success!')
+		cy.contains('h1', 'Tymoteusz Czech')
 	})
 
-	it('navigates to /about', () => {
-		cy.get('nav a').contains('about').click()
-		cy.url().should('include', '/about')
-	})
+	// it('navigates to /about', () => {
+	// 	cy.get('nav a').contains('about').click()
+	// 	cy.url().should('include', '/about')
+	// })
 
-	it('navigates to /blog', () => {
-		cy.get('nav a').contains('blog').click()
-		cy.url().should('include', '/blog')
-	})
+	// it('navigates to /blog', () => {
+	// 	cy.get('nav a').contains('blog').click()
+	// 	cy.url().should('include', '/blog')
+	// })
 })
